f8f03f4e4fddb5a5367c4a90bcc34cf7ef48cd14
Yi Jie <pangjocelyn2005@gmail.com>
2026-01-15 21:19:54 +0800

    Add participant queries

--- Backend/internal/sqlc/participants queries.sql
+++ Backend/internal/sqlc/participants queries.sql
@@ -12,41 +12,5 @@ FROM
 WHERE
   id = $1; 
 
---name CreateParticipant :one 
-INSERT INTO participants (
-  id,
-  user_id,
-  name,
-  email,
-  phone_number
-) VALUES (
-  $1,
-  $2,
-  $3,
-  $4,
-  $5
-) RETURNING *;
 
---name GetVolunteerByUserID :one
-SELECT
-  *
-FROM
-  users
-WHERE
-  user_id = $1 AND role = 'volunteer';  
-
-  --CreateVolunteer :one
-INSERT INTO users (
-  user_id,
-  name,
-  email,
-  phone_number,
-  role
-) VALUES (
-  $1,
-  $2,
-  $3,
-  $4,
-  'volunteer'
-) RETURNING *;  
 
